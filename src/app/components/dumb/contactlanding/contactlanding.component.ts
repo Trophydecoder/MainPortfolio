f41ac6a014c0f197b5639c231a86130926ff5d46
Trophydecoder <trophydecoder@gmail.com>
2025-09-01 13:27:57 +0200

    done with contact UI , only logic left

--- src/app/components/dumb/contactlanding/contactlanding.component.ts
+++ src/app/components/dumb/contactlanding/contactlanding.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { trigger, style, transition, animate, query, stagger } from '@angular/animations';
+
 
 @Component({
   selector: 'app-contactlanding',
   imports: [],
   templateUrl: './contactlanding.component.html',
-  styleUrl: './contactlanding.component.scss'
+  styleUrl: './contactlanding.component.scss',
+  animations: [
+    trigger('scrollAnimation', [
+      transition(':enter', [ // <-- triggers when element is inserted
+        query('.fade-in', [
+          style({ opacity: 0, transform: 'translateY(30px)' }),
+          stagger(200, [
+            animate('8000ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+          ])
+        ], { optional: true }),
+      ])
+    ])
+  ]
 })
 export class ContactlandingComponent {
 
